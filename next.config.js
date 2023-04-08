5c07a047619579f444f30222b21df51effb1d6ac
Stablest <marlonlocci@hotmail.com>
2023-04-07 22:05:00 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-// next.config.js
-// module.exports = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
-//   },
-// };
-
+// next.config.js;
 module.exports = {
   images: {
-    domains: ["drive.google.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
